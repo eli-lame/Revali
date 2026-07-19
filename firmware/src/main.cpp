a30ed00f457b7ed88253b4e891a3debf8dd9fd51
eli-lame <eli.lamothe@gmail.com>
2026-07-19 12:39:42 -0700

    main.cpp successful build with headers

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,11 +1,14 @@
 #include <Arduino.h>
+#include "estimator/IStateEstimator.h"
+#include "drivers/IImuSource.h"
 
-void setup()
-{
+using namespace charon;
+
+void setup() {
     Serial.begin(115200);
+    VehicleState s;              // default-constructed, all zeros
     Serial.println("Flight Core starting...");
+    Serial.printf("state valid=%d yaw=%.2f\n", s.valid, s.yaw_rad);
 }
 
-void loop()
-{
-}
+void loop() {}
